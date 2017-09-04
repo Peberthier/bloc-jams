81c57bbe645c41687c701cf1b7853cba537bfe3a
Pierre <peberthier@gmail.com>
2017-09-04 08:42:25 +0200

    each attempt correction

--- BLOC/bloc-jams/scripts/utilities.js
+++ BLOC/bloc-jams/scripts/utilities.js
@@ -1,26 +1,5 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function(points) {
-
-  var revealPoint = function(x) {
-      x.style.opacity = 1;
-      x.style.transform = "scaleX(1) translateY(0)";
-      x.style.msTransform = "scaleX(1) translateY(0)";
-      x.style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-  points.forEach(revealPoint())
-};
-
-window.onload = function() {
-  if (window.innerHeight > 950) {
-        animatePoints(pointsArray);
-    }
-   var sellingPoints = document.getElementsByClassName('selling-points')[0];
-   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-
-  window.addEventListener('scroll', function(event) {
-    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-              animatePoints(pointsArray);
-          }
-});
+function forEach (array, callback) {
+  for (var i = 0; i < array.length; i++) {
+    callback (array[i]);
+  }
 }
